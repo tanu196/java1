e4afabfff56e75e091554219a9318d9195bf11e5
Iku <bsi26006@uhe.ac.jp>
2026-07-06 14:40:33 +0900

    seven commit

---
+++ 練習１/src/練習１/Program.java
@@ -0,0 +1,22 @@
+package 練習１;
+
+public class Program {
+
+	public static void main(String[] args) {
+//		System.out.println("1. mainから始まる");
+//		System.out.println("2.上から順に実行される");
+//		
+//		System.out.println("Hel\"lo");
+//		
+//		System.out.println("1:\t入\t金");
+//		System.out.println("2:\t出\t金");
+//		System.out.println("3:\t残高\t確認");
+		
+		
+		System.out.println(0b1010001);
+		System.out.println(10e3);
+		
+		final int MAX = 100;
+	}
+
+}
