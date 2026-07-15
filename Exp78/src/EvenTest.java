d7ac2b1354728366786b3013aecd3bb334c183a8
Iku <bsi26006@uhe.ac.jp>
2026-07-15 15:59:12 +0900

    thirteen commit

--- Exp78/src/EvenTest.java
+++ Exp78/src/EvenTest.java
@@ -2,10 +2,20 @@
 // 10回まわる。表示されるのは何個？
 class EvenTest {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) {               // 偶数だけ
-                System.out.print(i + " ");
-            }
+//    	String a = "e";
+//        for (int i = 0; i < 20; i++) {
+//            if (i % 3 == 0 || Integer.toString(i).contains("3")) {               // 偶数だけ
+//                System.out.print(i + " ");
+//            }
+//        }
+        
+        for(int i = 1 ; i <= 9 ; i++) {
+        	for(int j = 1 ; j <= 9 ; j++) {
+        		System.out.print(i*j + " ");
+        	}
+        	System.out.println();
         }
+        	
+        
     }
 }
--- Exp78/src/TriangleTest.java
+++ Exp78/src/TriangleTest.java
@@ -8,5 +8,15 @@ class TriangleTest {
             }
             System.out.println();
         }
+        
+        
+//        for(int i = 1 ; i <= 4 ; i--) {
+//        	for(int j = 0 ; j < i ; j--) {
+//        		System.out.print("*");
+//        	}
+//        	System.out.println();
+//        }
+        
+        
     }
 }
