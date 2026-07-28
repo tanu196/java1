b19bdd34b76f2f6f401233f78078e2e67ea5b199
Iku <bsi26006@uhe.ac.jp>
2026-07-28 12:14:48 +0900

    twenty-two commit

---
+++ Pce/src/Teach1.java
@@ -0,0 +1,13 @@
+public class Teach1 {
+	public static void main(String[] args) {
+		
+//		
+//		System.out.println("計算結果：" + 50 + 50);
+//		System.out.println("計算結果：" + (10 + 90));
+//		System.out.println(("計算結果：" + 50) + 50);
+//		System.out.println("計算結果：" + 1 + 0 + 0);
+		System.out.println("計算結果：" + 500 / 5);
+
+	}
+
+}
